eee7311154aa6407baf9328527f9f93dd5e2d345
JuneBuug <junee613@gmail.com>
2019-07-14 18:39:07 +0900

    [생성] 프로젝트 세팅

---
+++ src/main/java/com/sleep/studyboot/StudybootApplication.java
@@ -0,0 +1,13 @@
+package com.sleep.studyboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudybootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudybootApplication.class, args);
+    }
+
+}
