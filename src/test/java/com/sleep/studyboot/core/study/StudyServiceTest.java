e86356c16c2290a2c16850f72c57352ce004ce0c
JuneBuug <junee613@gmail.com>
2019-07-16 23:17:05 +0900

    [수정] unused import 제거

--- src/test/java/com/sleep/studyboot/core/study/StudyServiceTest.java
+++ src/test/java/com/sleep/studyboot/core/study/StudyServiceTest.java
@@ -8,9 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @ExtendWith(MockitoExtension.class)
 public class StudyServiceTest {
