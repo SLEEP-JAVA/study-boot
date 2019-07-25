2eeb8a4e8410f566677640e8668a9fbd622e1284
june-y-kim <june.y.kim@navercorp.com>
2019-07-25 14:59:26 +0900

    [개발] home FE 개발 - 메인이미지 추가

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    
+  <div class="main">
+    <p> STUDY BOOT</p>
+    <img class="main-img" alt="메인이미지" src="../assets/main.png"/>
   </div>
 </template>
 
@@ -9,14 +10,24 @@
     name: 'Home',
     data() {
       return {
-        activeIndex: '1',
-        activeIndex2: '1'
       };
     },
     methods: {
-      handleSelect(key, keyPath) {
-        console.log(key, keyPath);
-      }
+
     }
   }
 </script>
+
+<style>
+
+  .main {
+    max-width: 100%;
+    font-size: 2em;
+    font-weight: 900;
+    font-family: SansSerif, Helvetica Neue;
+  }
+
+  .main-img {
+    max-width: 100%;
+  }
+</style>
