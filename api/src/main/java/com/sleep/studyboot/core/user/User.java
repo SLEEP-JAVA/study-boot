76c3dc3faaf0845cd8c608cd8e6745f6361408aa
minzzzy <minpjh@gmail.com>
2019-08-01 19:33:07 +0900

    ISSUE-13 닉네임 삭제 및 테스트 코드 추가

--- api/src/main/java/com/sleep/studyboot/core/user/User.java
+++ api/src/main/java/com/sleep/studyboot/core/user/User.java
@@ -2,6 +2,7 @@ package com.sleep.studyboot.core.user;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -9,14 +10,12 @@ import javax.persistence.*;
 @Entity
 @Table(name = "user")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "nick_name")
-    private String nickname;
 
     @Column(name = "email")
     private String email;
@@ -28,16 +27,14 @@ public class User {
     private String avartarUrl;
 
     @Builder
-    public User(String nickname, String email, String name, String avartarUrl) {
-        this.nickname = nickname;
+    public User(String email, String name, String avartarUrl) {
         this.email = email;
         this.name = name;
         this.avartarUrl = avartarUrl;
     }
 
-    public User updateByGithub(String name, String snsId, String avartarUrl) {
+    public User updateByGithub(String name, String avartarUrl) {
         this.name = name;
-        this.nickname = snsId;
         this.avartarUrl = avartarUrl;
 
         return this;
